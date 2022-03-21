247f759f305281d98de3c71092b5c7b76cdf8feb
flowscode <joshuadawkins95@gmail.com>
2022-03-21 12:05:25 +0000

    updated class and add method for fullname

--- classes.py
+++ classes.py
@@ -1,22 +1,18 @@
 class Employee:
-    pass
+    def __init__(self, first, last, age, pay):
+        self.first = first
+        self.last = last
+        self.age = age
+        self.pay = pay
+        self.email = first + "." + last + "@GMAIL.COM"
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
 
 
-emp_1 = Employee()
-emp_2 = Employee()
-
-print(emp_1)
-print(emp_2)
-
-emp_1.first = "Joshua"
-emp_1.last = "Dawkins"
-emp_1.age = 27
-emp_1.email = "jd@gmail.com"
-
-emp_2.first = "Liam"
-emp_2.last = "May"
-emp_2.age = 7
-emp_2.email = "lmay@gmail.com"
+emp_1 = Employee("Joshua", "Dawkins", 27, 30000)
+emp_2 = Employee("Liam", "May", 25, 78000)
 
 print(emp_1.first)
-print(emp_2)
+print(emp_2.email)
+print(emp_2.fullname())

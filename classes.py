a091af62511be67c78eb53d1ef0c3651e3c468ad
flowscode <joshuadawkins95@gmail.com>
2022-03-21 11:49:53 +0000

    classes refresher

---
+++ classes.py
@@ -0,0 +1,22 @@
+class Employee:
+    pass
+
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+emp_1.first = "Joshua"
+emp_1.last = "Dawkins"
+emp_1.age = 27
+emp_1.email = "jd@gmail.com"
+
+emp_2.first = "Liam"
+emp_2.last = "May"
+emp_2.age = 7
+emp_2.email = "lmay@gmail.com"
+
+print(emp_1.first)
+print(emp_2)
